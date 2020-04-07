04e1942c187cf16c40eca5e8ab388f2458815d98
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-04-07 15:24:58 +0200

    Removes unused files

--- fluffy/workflows/make_model.py
+++
@@ -1,9 +0,0 @@
-"""Code for making a model"""
-
-import logging
-import pathlib
-from typing import Iterator
-
-from fluffy.commands.preface import get_preface_cmd
-
-LOG = logging.getLogger(__name__)
--- fluffy/workflows/make_ref.py
+++
@@ -1,6 +0,0 @@
-"""Workflow for making a reference based on samples"""
-
-import pathlib
-from typing import Iterator
-
-from slurmpy import Slurm
