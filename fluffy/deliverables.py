74574495d2868aa3b53909036eda24a0e568d2e2
Jesper Eisfeldt <Jesper@n174-p247.eduroam.kth.se>
2020-11-19 12:57:50 +0100

    modified:   fluffy/deliverables.py

--- fluffy/deliverables.py
+++ fluffy/deliverables.py
@@ -21,6 +21,11 @@ def print_deliverables(output_dir: Path,project_dir: Path, samples: list) -> Non
     deliverables["files"].append({"format":"csv", "id":project_name,"path":str(summary_path),"step":"summarise_batch","tag":"NIPT_csv"})
     deliverables["files"].append({"format":"html", "id":project_name,"path":str(multiqc_path),"step":"summarise_batch","tag":"MultiQC"})
 
+    for sample in samples:
+        sample_id=sample["sample_id"]
+        zip_path=output_dir.absolute() / f"{sample_id}.WCXpredict_abberations.filt.bed"
+        deliverables["files"].append({"format":"bed", "id":sample_id,"path":str(zip_path),"step":"wcx","tag":"Wisecondor_abberations"})
+
     f=open(f"{str(output_dir)}/deliverables.yaml","w")
     f.write(yaml.dump(deliverables))
     f.close()
