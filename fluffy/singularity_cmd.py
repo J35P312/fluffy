e6aa013b8d5ec19ef6b6ba04fa8ee42c849d587b
jesper <jesper.eisfeldt@scilifelab.se>
2021-01-26 17:45:04 +0100

    modified:   singularity_cmd.py

--- fluffy/singularity_cmd.py
+++ fluffy/singularity_cmd.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 
 def singularity_base(singularity_exe: str, out_dir: Path, project_dir: Path, singularity_bind: list):
-	bind=f" --bind {str(out_dir)} --bind {str(project_dir)} " + "--bind".join(singularity_bind)
+	bind=f" --bind {str(out_dir)} --bind {str(project_dir)} " + " --bind".join(singularity_bind)
 	singularity_base_cmd= f"singularity exec {bind} {singularity_exe}"
 	return singularity_base_cmd
 
