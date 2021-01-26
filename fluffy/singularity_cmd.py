a4988dfb78e2582fd5fa8d903d6899be0f798eb8
jesper <jesper.eisfeldt@scilifelab.se>
2021-01-26 17:25:47 +0100

    new file:   fluffy/singularity_cmd.py

---
+++ fluffy/singularity_cmd.py
@@ -0,0 +1,9 @@
+"""code to generate the singularity base command"""
+import logging
+from pathlib import Path
+
+def singularity_base(singularity_exe: str, out_dir: Path, project_dir: Path, singularity_bind: list):
+	bind=f" --bind {str(out_dir)} --bind {str(project_dir)} " + "--bind".join(singularity_bind)
+	singularity_base_cmd= f"singularity exec {bind} {singularity_exe}"
+	return singularity_base_cmd
+
