b5aca0c68782c6f2d884f1bf0ee50cc487d6a03c
Jesper Eisfeldt <jesper.eisfeldt@hasta.scilifelab.se>
2021-02-16 13:53:57 +0100

    modified:   fluffy/version.py 	modified:   fluffy/workflows/status_update.py

--- fluffy/version.py
+++ fluffy/version.py
@@ -1,3 +1,3 @@
 """Holds current version of fluffy"""
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
--- fluffy/workflows/status_update.py
+++ fluffy/workflows/status_update.py
@@ -7,10 +7,13 @@ from fluffy.slurm_api import SlurmAPI
 LOG = logging.getLogger(__name__)
 
 def sed_replace_inplace(
-    in_filename: str, find_str: str, replace_str: str
+    in_filename: str,project_dir:str ,flag: str, find_str: str, replace_str: str
 ) -> str:
     """create a command for running sed find and replace inplace"""
-    cmd = f"sed -i \'s/{find_str}/{replace_str}/g\' {in_filename}"
+    cmd = (
+        f"sed -i \'s/{find_str}/{replace_str}/g\' {in_filename}\n"
+        f"echo {replace_str} > {project_dir}/{flag}"
+    )
     return cmd
 
 def pipe_complete(
@@ -23,7 +26,7 @@ def pipe_complete(
     out_dir = configs["out"]
 
     sed_replace_inplace_cmd = sed_replace_inplace(
-        in_filename= str(out_dir / "analysis_status.json"), find_str=" \"running\"", replace_str=" \"complete\""
+        in_filename= str(out_dir / "analysis_status.json"),project_dir=str(out_dir),flag="COMPLETE", find_str=" \"running\"", replace_str=" \"complete\""
     )
 
     jobid = slurm_api.run_job(
@@ -45,7 +48,7 @@ def pipe_fail(
     out_dir = configs["out"]
 
     sed_replace_inplace_cmd = sed_replace_inplace(
-        in_filename= str(out_dir / "analysis_status.json"), find_str=" \"running\"", replace_str=" \"fail\""
+        in_filename= str(out_dir / "analysis_status.json"),project_dir=str(out_dir),flag="FAIL", find_str=" \"running\"", replace_str=" \"fail\""
     )
 
     jobid = slurm_api.run_job(
